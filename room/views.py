fa69bb41b42717f608086954f5a8482227995cf2
Nikx908 <nikhilparbat908@gmail.com>
2024-03-11 16:40:26 +0530

    Create Room Api

--- room/views.py
+++ room/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
 from .serializers import RoomSerializer, MessageSerializer
 from .models import Room, Message
@@ -31,3 +32,14 @@ def new_message(request, room_slug):
             serializer.save(room=room, user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def room_create(request):
+    serializer = RoomSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors)
+    
