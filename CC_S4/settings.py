9b9dbb04fb8c102e26bf592ea3a862c1c6bf3eb5
Nikx908 <nikhilparbat908@gmail.com>
2024-03-17 03:21:05 +0530

    database changed

--- CC_S4/settings.py
+++ CC_S4/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'CC_S4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'pwgquhvs',
-#        'USER': 'pwgquhvs',
-#        'PASSWORD': '40190V0BXHsMxra3VuMMTM1C521dAfU5',
-#        'HOST': 'floppy.db.elephantsql.com',
-#        'PORT': '5432',
-#    }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'pwgquhvs',
+       'USER': 'pwgquhvs',
+       'PASSWORD': '40190V0BXHsMxra3VuMMTM1C521dAfU5',
+       'HOST': 'floppy.db.elephantsql.com',
+       'PORT': '5432',
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
