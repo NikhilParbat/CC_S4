b818f7b7f2aeddad09a5aca493ce58b720736cef
Nikx908 <nikhilparbat908@gmail.com>
2024-03-17 01:43:32 +0530

    Urls changes

--- CC_S4/urls.py
+++ CC_S4/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('r/',include('room.urls')),
     path('',include('core.urls')),
+    path('',include('room.urls')),
+    path('admin/', admin.site.urls),
     
 ]
